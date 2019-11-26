9ec92fcbfeb1d185591b888326456985d90e7589
Megan Ennis <Megan@Megans-iMac-9.local>
2019-11-26 13:39:49 +0000

    Basic login endpoint

--- authentication/auth-router.js
+++ authentication/auth-router.js
@@ -14,12 +14,41 @@ router.post("/register", validateUser, (req, res) => {
   users
     .add(newUser)
     .then(user => {
-      res.status(201).json({ message: "Account created.", user });
+      res
+        .status(201)
+        .json({
+          message: "Account created.",
+          user: { id: user.id, username: user.username }
+        });
     })
     .catch(err => {
       res
         .status(500)
         .json({ message: "Could not add new user: " + err.message });
+    });
+});
+
+router.post("/login", validateUser, (req, res) => {
+  const { username, password } = req.body;
+
+  users
+    .findByUsername(username)
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res
+          .status(200)
+          .json({
+            message: `Welcome back ${user.username}`,
+            user: { id: user.id, username: user.username }
+          });
+      } else {
+        res.status(401).json({
+          message: "Could not find user. Invalid credentials provided"
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Could not login user: " + err.message });
     });
 });
 
